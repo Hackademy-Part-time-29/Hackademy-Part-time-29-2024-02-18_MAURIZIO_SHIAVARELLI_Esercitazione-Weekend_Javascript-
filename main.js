ca929b78c01c1a1ccbf84879d1fd8ce4c5076aec
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-02-18 16:09:40 +0100

    inizio esercitazione

---
+++ main.js
@@ -0,0 +1,17 @@
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+
+// console.log della somma delle 5 variabili(let) numero
+
+let somma = numero1 + numero2 + numero3 + numero4 + numero5;
+console.log(somma)
+
+// console.log della media delle 5 variabili(let) numero
+let media = (numero1 + numero2 + numero3 + numero4 + numero5) / numero5;
+console.log(media)
+
+
+
