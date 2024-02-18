5a270d8e644429e04a865c8a15716620665b3a74
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-02-18 17:14:03 +0100

    fine terzo esercizio

--- main.js
+++ main.js
@@ -1,17 +1,84 @@
-let numero1 = 1
-let numero2 = 2
-let numero3 = 3
-let numero4 = 4
-let numero5 = 5
+//esercizio numero 1
+
+let numero1 = 3;
+let numero2 = 4;
+let numero3 = 8;
+let numero4 = 50;
+let numero5 = 15;
+
+let numeroelementi = 5
 
 // console.log della somma delle 5 variabili(let) numero
 
 let somma = numero1 + numero2 + numero3 + numero4 + numero5;
-console.log(somma)
+console.log(somma);
 
 // console.log della media delle 5 variabili(let) numero
-let media = (numero1 + numero2 + numero3 + numero4 + numero5) / numero5;
-console.log(media)
+let media = (numero1 + numero2 + numero3 + numero4 + numero5) / numeroelementi;
+console.log(media);
+
+// console.log della somma e della media in due stringhe
+// console.log("la somma tra i numeri equivale a " +somma)
+// console.log("la media tra i numeri é " + media)
+
+// console.log dell somma e della media in una stringa
+console.log("la somma tra i numeri euqivale a " + somma + " mentre la media tra i numeri é " + media)
+
+
+
+
+
+
+//esercizio numero 2
+
+let annoCorrente = 2024;
+let etadiNaomi = 1997;
+let centoAnni = 100;
+let anniDiNaomi= (annoCorrente-etadiNaomi);
+
+
+//anni di naomi = 27
+console.log(annoCorrente-etadiNaomi);
+let anninaomi = (annoCorrente-etadiNaomi);
+
+//anni per arrivar a cento
+console.log(centoAnni-anniDiNaomi);
+let anniPerArrivareACento = (centoAnni-anniDiNaomi)
+
+console.log("hai "+ anninaomi + " e ti mancano " + anniPerArrivareACento +" anni per arrivare a cento")
+
+
+
+
+
+// esercizio numero 3
+
+// let variabile1 = 10;
+// let variabile2 = 5;
+
+// let somma1 = variabile1+variabile2;
+// let sottrazione1 = variabile1-variabile2;
+// let moltiplicazione1 = variabile1*variabile2;
+// let divisione1 = variabile1/variabile2;
+
+// console.log( "in base ai numeri scelti i risultati delle varie operazioni sono : somma ="+somma1 +" sottrazione= " + sottrazione1+ " moltiplicazione= "+ moltiplicazione1 + " divisione= "+divisione1 )
+
+
+let variabile1 = parseInt(prompt("inserisci un numero"));
+let variabile2 = parseInt(prompt("inserisci un numero"));
+
+let somma1 = variabile1+variabile2;
+let sottrazione1 = variabile1-variabile2;
+let moltiplicazione1 = variabile1*variabile2;
+let divisione1 = variabile1/variabile2;
+
+console.log( "in base ai numeri scelti i risultati delle varie operazioni sono : somma ="+somma1 +" sottrazione= " + sottrazione1+ " moltiplicazione= "+ moltiplicazione1 + " divisione= "+divisione1 );
+
+
+
+
+
+
 
 
 
